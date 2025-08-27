32a165876637fd583eea5f6c7d57e9aca4e710bc
Sahil Shrivastava <sahilshekhar84@gmail.com>
2025-08-27 19:43:26 +0530

    check basic husky setup

--- src/setting/Setting.tsx
+++ src/setting/Setting.tsx
@@ -1,18 +1,18 @@
-import React from 'react'
-import { View, Text, StyleSheet } from 'react-native'
+import React from 'react';
+import { View, Text, StyleSheet } from 'react-native';
 
 const Setting = () => {
-    return (
-        <View style={styles.container}>
-            <Text>Setting</Text>
-        </View>
-    );
+  return (
+    <View style={styles.container}>
+      <Text>Setting</Text>
+    </View>
+  );
 };
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
 });
-export default Setting
+export default Setting;
