552efba369817875e76b314e64e5559501c216bf
Sahil Shrivastava <sahilshekhar84@gmail.com>
2025-09-06 10:45:49 +0530

    try to setup some rule for code , hint:2

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,8 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    // "no-console": ["error", { allow: ["warn", "error"] }], // Uncomment to disallow console.log, allow console.warn and console.error
+    'no-console': 'error',
+  },
 };
