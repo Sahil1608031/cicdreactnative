11a459b7e99be81767ccac6216d1bf541a770b45
Sahil Shrivastava <sahilshekhar84@gmail.com>
2025-09-06 21:23:07 +0530

    removing unusecode , setup sonar hit :2

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -1,11 +1,7 @@
-import React, { JSX, useEffect } from 'react';
+import React, { JSX } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
-import { testAllRules } from '../utilites/dummy';
 
 const Home = (): JSX.Element => {
-  useEffect(() => {
-    testAllRules(1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11);
-  }, []);
   return (
     <View style={styles.container}>
       <Text>Home</Text>
