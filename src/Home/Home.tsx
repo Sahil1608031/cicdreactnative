a84b1fa09eb0d052bde06436c78bd970e153612e
Sahil Shrivastava <sahilshekhar84@gmail.com>
2025-09-07 11:05:52 +0530

    sonar test hint 2

--- src/Home/Home.tsx
+++ src/Home/Home.tsx
@@ -2,7 +2,7 @@ import React, { JSX, useState } from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 //home compoent
 const Home = (): JSX.Element => {
-  const [data, setData] = useState<string>('');
+  const [data, setData] = useState<any>('');
 
   return (
     <View style={styles.container}>
